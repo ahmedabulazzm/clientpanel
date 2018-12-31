bc38efba17ed4baa7c64a622a8896538452f0604
ahmedabulazzm <ahmed.abulazm1@gmail.com>
2018-12-31 13:53:50 +0200

    Initial commit

---
+++ src/components/clients/Clients.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Clients extends Component {
+  render() {
+    const clients = [
+      {
+        id: "32131231",
+        firstName: "Kevin",
+        lastName: "Johnson",
+        email: "kevin@gmail.com",
+        phone: "123-123-123",
+        balance: "30"
+      },
+      {
+        id: "445523324",
+        firstName: "Bob",
+        lastName: "Jackson",
+        email: "bob@gmail.com",
+        phone: "222-222-222",
+        balance: "50"
+      },
+      {
+        id: "99999999",
+        firstName: "Jack",
+        lastName: "TheRipper",
+        email: "yourboyjack@gmail.com",
+        phone: "555-555-555",
+        balance: "0"
+      }
+    ];
+
+    if (clients) {
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <h2>
+                <i className="fas fa-users" /> Clients
+              </h2>
+            </div>
+            <div className="col-md-6" />
+          </div>
+          <table className="table table-striped">
+            <thead className="thead-inverse">
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Balance</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {clients.map(client => (
+                <tr key={client.id}>
+                  <td>
+                    {client.firstName} {client.lastName}
+                  </td>
+                  <td>{client.email}</td>
+                  <td>${parseFloat(client.balance).toFixed(2)}</td>
+                  <td>
+                    <Link
+                      to={`/clients/${client.id}`}
+                      className="btn btn-secondary btn-sm"
+                    >
+                      <i className="fas fa-arrow-circle-right" /> Details
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      );
+    } else {
+      return <h1>Loading...</h1>;
+    }
+  }
+}
+
+export default Clients;
